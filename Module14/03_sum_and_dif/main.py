8c72be4e98bd106f823aa2efdee6025392619e8d
Novikov Petr <baksik.bz@gmail.com>
2022-06-14 15:01:21 +0300

    3 задание выполнено

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+# ЗАДАЧА 3
+def sum_n() -> object:
+  S = int(sum(map(int,str(N))))
+  return S
+def col_chair():
+    M = str(N)
+    Col= 0
+    for i in M:
+        Col += 1
+    b = int(Col)
+    return b
+
+
+N = int(input('Введите число: '))
+print('\nCумма чисел:', sum_n())
+print('Количество цифр в числе:',col_chair())
+print('Разность суммы и количества цифр: ',sum_n()-col_chair())
