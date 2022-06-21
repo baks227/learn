e11262ecc5a87bee3bf4ed5c6a9a5edd01ef04ec
Novikov Petr <baksik.bz@gmail.com>
2022-06-21 12:41:41 +0300

    7 задание выполнено

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+# ЗАдача 7
+def search():
+  j,k,a = 0,0,''
+
+  y1 = int(input('Введите первый год: '))
+  y2 = int(input('Введите второй год: '))
+  print('\nГоды от',y1,'до',y2,'с тремя одинаковыми цифрами:')
+  while y1 != y2:
+    for i in str(y1):
+        a = i
+        for k in str(y1):
+          if a == k:
+            j += 1
+        if j == 3:
+          print(y1)
+          break
+        j = 0
+    y1 += 1
+search_year()
