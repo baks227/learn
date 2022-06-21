672909b800340e55580b50c94346066269824062
Novikov Petr <baksik.bz@gmail.com>
2022-06-21 12:41:29 +0300

    6 задание выполнено

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+# ЗАдача 6
+def search(x,y,r):
+  if -r <= x <= r and -r <= y <= r:
+    print('Монетка где-то рядом')
+  else:
+    print('Монетки в области нет')
+print('Введите координаты монетки: ')
+x = float(input('\nX: '))
+y = float(input('Y: '))
+r = float(input('Введите радиус: '))
+search(x,y,r)
