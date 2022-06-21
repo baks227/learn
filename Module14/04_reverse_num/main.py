43ac8cdd420a0e597a0672c4f92a3857071cf237
Novikov Petr <baksik.bz@gmail.com>
2022-06-21 12:41:13 +0300

    4 задание выполнено

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,15 @@
-# TODO здесь писать код
+# Задача 4
+
+def reverse_n(n,word):
+  m = ".".join(map("".join, map(reversed, n.split("."))))
+
+  print(word+'наоборот: ',m)#если нули не нужны, меняем m на int(m)
+  return m
+n1 = input('Введите первое число: ')
+n2 = input('Введите второе число: ')
+sum = float(reverse_n(n1,'\nПервое число '))
+sum += float(reverse_n(n2,'Второе число '))
+print('Cумма: ',sum)
+
 
 
