adc67a1749063416254dd44b08bb5fe7e76ba13f
Novikov Petr <baksik.bz@gmail.com>
2022-09-27 16:16:10 +0300

    задание выполнено

--- Module16/06_unique_elems/main.py
+++ Module16/06_unique_elems/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+def gen_list (n,num):
+    generation = []
+    for i in range(n):
+        print('Введите',i+1,'число для',num,'списка: ',end='')
+        count = int(input())
+        generation.append(count)
+    return generation
+def gen_new_lists():
+    i = 0
+    sum_list = first_list + second_list
+    while i != len(sum_list):
+        for x in sum_list:
+            if sum_list.count(x) != 1:
+                sum_list.remove(x)
+        i += 1
+    return sum_list
+first_list = gen_list(3,'первого')
+second_list = gen_list(7,'второго')
+uni_list = gen_new_lists()
+print('\nПервый список:',first_list)
+print('Второй список',second_list)
+print('\nНовый первый список с уникальными элементами:',uni_list)
