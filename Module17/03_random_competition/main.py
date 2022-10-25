4462ab5017787848912cfe96c7055dee3f1c2efd
Novikov Petr <baksik.bz@gmail.com>
2022-10-25 16:03:51 +0300

    задание выполнено

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import random
+
+first_team = [round(random.uniform(5,10),2) for _ in range(20)]
+second_team = [round(random.uniform(5,10),2) for _ in range(20)]
+winners = [(first_team[x] if first_team[x] > second_team[x] else second_team[x])
+           for x in range(len(first_team))]
+
+print('Первая команда: ',first_team,'\nВторая команда: ',
+      second_team,'\nПобедители тура:',winners)
