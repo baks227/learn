fc75967b2202d338b0281d1ec775c9942e2979b1
Novikov Petr <baksik.bz@gmail.com>
2022-10-26 14:57:03 +0300

    задание выполнено

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import random
+
+kolvo_chisel = int(input('Количество чисел в списке: '))
+
+nachalnyi_spisok = [random.randrange(0,3) for _ in range(kolvo_chisel)]
+last_spisok = [x for x in nachalnyi_spisok if x != 0]
+
+print('Список до сжатия: ',nachalnyi_spisok)
+print('Список после сжатия: ',last_spisok)
