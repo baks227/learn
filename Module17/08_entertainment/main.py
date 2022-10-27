908174d2053a4e046b11bda5ea3560765e317a70
Novikov Petr <baksik.bz@gmail.com>
2022-10-27 10:31:57 +0300

    задание выполнено

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+palki = ['I' for _ in range(int(input('Количество палок: ')))]
+
+for i in range(1,int(input('Количество бросков: ')) +1):
+    print('Бросок',i,end='')
+    for j in range(int(input('. Сбиты палки с номера '))-1,int(input('по номер: '))):
+        palki[j] = '.'
+
+print('Результат:',end='')
+for i in palki:
+    print(i,end='')
