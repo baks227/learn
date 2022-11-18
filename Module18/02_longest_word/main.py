d3146da01f2e9284c6721b60140394580b9a1ad2
Novikov Petr <baksik.bz@gmail.com>
2022-11-18 11:42:51 +0300

    задание выполнено

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+stroka = input('Введите строку: ')
+
+longer_str = max(stroka.split(),key=len)
+
+print(f'Самое длинное слово: {longer_str}\nДлина этого слова: {len(longer_str)}')
