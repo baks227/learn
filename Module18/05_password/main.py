94c297d832f944f441956867e44258db1cfff63a
Novikov Petr <baksik.bz@gmail.com>
2022-11-21 15:06:17 +0300

    задание выполнено

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+password = input('Придумайте пароль: ')
+
+while True:#and password.islower() and password.isupper()
+    if not password.isalnum() or sum(map(str.isupper,password)) <= 1 or len(password) < 7:
+        print('Пароль ненадёжный. Попробуйте ещё раз.')
+    else:
+        print('Это надёжный пароль!')
+        break
+    password = input('Придумайте пароль: ')
+
+
