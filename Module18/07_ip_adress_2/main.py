8c17c3dffc540e8ecb9964993b3d6bbf3bace71a
Novikov Petr <baksik.bz@gmail.com>
2022-11-22 16:46:49 +0300

    задание выполнено

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+ip = input('Введите IP-адрес: ')
+
+ip_mass = ip.split('.')
+
+if len(ip_mass) != 4:
+    print('Адрес — это четыре числа, разделённые точками.')
+else:
+    num,out = 0,0
+    for i in ip_mass:
+        if i.isdigit():
+            num += 1
+            if int(i)>255:
+                out += 1
+                print(f'{i} превышает 255')
+        else:
+            print(f'{i} - это не целое число.')
+    if num == 4 and out == 0:
+        print('IP-адрес корректен.')
