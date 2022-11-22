e192871dc3f6b63c2903b68a4a0df5dfb5cf3702
Novikov Petr <baksik.bz@gmail.com>
2022-11-22 16:22:12 +0300

    задание выполнено

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,6 +1,6 @@
 password = input('Придумайте пароль: ')
 
-while True:#and password.islower() and password.isupper()
+while True:
     if not password.isalnum() or sum(map(str.isupper,password)) <= 1 or len(password) < 7:
         print('Пароль ненадёжный. Попробуйте ещё раз.')
     else:
--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+message = str(input('Введите строку: '))
+cnt,message_upd = 1,''
+
+for i in range(len(message)):
+    if i == (len(message)-1):
+        message_upd += message[i]+str(cnt)
+    else:
+        if message[i] == message[i+1]:
+            cnt += 1
+        else:
+            message_upd += message[i]+str(cnt)
+            cnt = 1
+
+print('Закодированная строка: ',message_upd)
