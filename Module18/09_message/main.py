ab93ac8586082b7629d93982810a57e70529a659
Novikov Petr <baksik.bz@gmail.com>
2022-11-22 20:15:37 +0300

    задание выполнено

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+message = input('Сообщение:').split()
+
+new_message = []
+
+for word in message:
+    if word.isalpha():
+        new_message += ''.join(reversed(word))
+    else:
+        i = 0
+        for char in word:
+            if char.isalpha():
+                i += 1
+
+            else:
+                new_message += ''.join(reversed(word[0:i]))
+                new_message += ''.join(char)
+            if not char.isalpha():
+                new_message += ''.join(reversed(word[i+1:len(word)]))
+                break
+    new_message += ''.join(' ')
+print(''.join(new_message))
