c14cbe5d0faa56c743dc60adf90b8ac99b4d88c6
Novikov Petr <baksik.bz@gmail.com>
2022-11-23 12:09:31 +0300

    задание выполнено

--- Module18/10_the_truth/main.py
+++ Module18/10_the_truth/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+def encrypt(message, key):
+    translated,sdvig= '',''
+    for symbol in message:
+        if symbol in alfabet:
+            num = alfabet.find(symbol)
+            translated += alfabet[num - key]
+        else:
+            translated += symbol
+    n = 3
+    for word in translated.split():
+        new_word = ''
+        for i in range(len(word)):
+            new_word += (word[i - n % len(word)])
+        if new_word.endswith('/'):
+           n += 1
+        sdvig += new_word + ' '
+    return sdvig.replace('/','.\n').replace('(',"'").replace('+','*').replace('-',',').replace('..','--')
+
+input_text = 'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip'
+alfabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+print("i - ", 1, " -------\n", encrypt(input_text, 1))
