51ba4bcf0ff8b3c200e7e364a0ca5d2892b99c37
Novikov Petr <baksik.bz@gmail.com>
2023-01-16 15:04:02 +0300

    задание выполнено

--- Module19/02_geography/main.py
+++ Module19/02_geography/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+kolvo_country = int(input('Количество стран: '))
+s_dict = {}
+for i in range(kolvo_country):
+    value = input('{}-я страна: '.format(i + 1)).split()
+    for city in value[1:]:
+        s_dict[city] = value[0]
+for i in range(3):
+    city = input('\n{}-й город: '.format(i + 1))
+    country = s_dict.get(city)
+    if country:
+        print(f'Город {city} расположен в стране {country}.')
+    else:
+        print(f' По городу {city} данных нет.')
