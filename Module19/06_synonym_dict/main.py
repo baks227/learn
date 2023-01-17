6cb2a52556c3441d1f9a87c5e54298929d3b1841
Novikov Petr <baksik.bz@gmail.com>
2023-01-17 12:41:03 +0300

    задание выполнено

--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+kolvo = int(input('Введите количество пар слов: '))
+word_dict = {}
+for i in range(1,kolvo +1):
+    print(f'{i}-я пара:',end=' ')
+    word = input().lower().split(' — ')
+    print(word)
+    word_dict.update({word[0]:word[1]})
+while True:
+    word = input('Введите слово: ').lower()
+    if word == 'end':
+        break
+    elif word in word_dict:
+        print('Синоним: ',word_dict[word])
+    else:
+        print('Такого слова в словаре нет.')
