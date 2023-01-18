0cd97ce322fb1946cfe47ddd6b045cdff49b060d
Novikov Petr <baksik.bz@gmail.com>
2023-01-18 14:13:58 +0300

    Задание выполнено

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+word = input('Введите строку: ').lower()
+chars = set()
+
+for i in word:
+    if i in chars:
+        chars.remove(i)
+    else:
+        chars.add(i)
+print(('Можно', 'Нельзя')[len(chars) > 1], 'сделать полиндром')
