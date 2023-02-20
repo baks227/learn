d22c01a40c8288bd8162d8b06f2d4e65fdaa4a98
Novikov Petr <baksik.bz@gmail.com>
2023-02-20 16:46:21 +0300

    Задание выполнено.

--- Module20/05_one_family/main.py
+++ Module20/05_one_family/main.py
@@ -6,6 +6,6 @@ age_dict = {
 
 familya = input('Введите фамилию: ').lower()
 for i_person in age_dict:
-    if familya.lower() in i_person[0].lower() or familya+'а' in i_person[0].lower() \
+    if familya in i_person[0].lower() or familya+'а' in i_person[0].lower() \
             or familya[:len(familya)-1] in i_person[0].lower():
         print(i_person[0],i_person[1],age_dict[i_person])
