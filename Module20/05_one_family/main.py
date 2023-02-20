6d75c85909c298a77cc2312147988d6409ce1a30
Novikov Petr <baksik.bz@gmail.com>
2023-02-20 16:45:56 +0300

    Задание выполнено.

--- Module20/05_one_family/main.py
+++ Module20/05_one_family/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+age_dict = {
+    ('Сидоров', 'Никита'): 35,
+('Сидорова', 'Алина'): 34,
+('Сидоров', 'Павел'): 10,
+}
+
+familya = input('Введите фамилию: ').lower()
+for i_person in age_dict:
+    if familya.lower() in i_person[0].lower() or familya+'а' in i_person[0].lower() \
+            or familya[:len(familya)-1] in i_person[0].lower():
+        print(i_person[0],i_person[1],age_dict[i_person])
