b448c60fec39c0a9f1cd64b1c681c99a9aadb6d7
Novikov Petr <baksik.bz@gmail.com>
2023-02-20 12:45:33 +0300

    Задание выполнено.

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def is_prime(i_index):
+    count = 0
+    if i_index > 1:
+        for i_num in range(2, i_index // 2 + 1):
+            if (i_index % i_num) == 0:
+                count += 1
+        if count == 0:
+            return True
+    else:
+        return False
+
+def crypto(text):
+    return [i_sym for i_index, i_sym in enumerate(text) if is_prime(i_index)]
+
