704b31fc0a09a1a6ca5d0a0988feff0d4bfdbdf2
Novikov Petr <baksik.bz@gmail.com>
2023-02-20 16:17:10 +0300

    Задание выполнено. Прошу обратить внимание, что в моем случае нужно взять кортеж в еще одни скобки!!! #print(slicer(((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10),2))) Для того, чтобы переменная являлась кортежом, а не 2 переменные задавались при вызове функции.

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def slicer(cort):
+    if len(cort) == 2:
+        sym = cort[1]
+        cort = cort[0]
+    else:
+        return []
+    if cort.count(sym) == 1:
+        return cort[cort.index(sym):]
+    return cort[cort.index(sym):cort.index(sym,cort.index(sym)+1)+1]
+
+
+
+
+#print(slicer(((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10),2)))
