c74f2f5684229b6b10eefa3499de477cfa11fa8b
Novikov Petr <baksik.bz@gmail.com>
2023-04-03 17:13:51 +0300

    Задание выполнено.

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+def competition_protocol():
+    records = list()
+    records_num = int(input('Сколько записай вносится в протокол? '))
+    print('Записи (Результат и имя) - введите через пробел')
+    for attempt in range(records_num):
+        score, name = input(f'{attempt + 1}-я запись: ', ).split()
+        records.append((score,records_num - attempt, name))
+    result(sorted(records))
+
+def result(protocol):
+    players, top, place = {}, set(), 1
+    print('\nИтоги соревнований: ')
+    for id_rec, record in enumerate(protocol[::-1]):
+        score, _, name = record
+        if name not in players or int(score) > int(players[name]):
+            players[name] = score
+    for name, score in players.items():
+        if name not in top and place <= 3:
+            top.add(name)
+            print(f"{place}-е место: {name} ({score})")
+            place += 1
+        elif place >3:
+            break
+
+competition_protocol()
