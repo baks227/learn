871e3eacea6a221ada675208a4b797f4532da294
Novikov Petr <baksik.bz@gmail.com>
2023-04-24 12:26:27 +0300

    Задание выполнено.

--- Module21/02_search_element_2/main.py
+++ Module21/02_search_element_2/main.py
@@ -10,3 +10,29 @@ site = {
 		}
 	}
 }
+def find_key(struct,key):
+	if key in struct:
+		return struct[key]
+	depth = input('Хотите ввести максимальную глубину? Y/N: ')
+	if depth == 'y' or depth =='Y':
+		user_depth = int(input('Введите максимальную глубину: '))
+		for sub_struct in struct.values():
+			if user_depth == 0:
+				result = find_key(sub_struct,key)
+				if result:
+					break
+			user_depth -= 1
+		else:
+			result = None
+	else:
+		for sub_struct in struct.values():
+			if isinstance(sub_struct,dict):
+				result = find_key(sub_struct,key)
+				if result:
+					break
+		else:
+			result = None
+	return result
+user_key = input('Введите искомый ключ: ')
+value = find_key(site,user_key)
+print('Значение ключа:',value)
