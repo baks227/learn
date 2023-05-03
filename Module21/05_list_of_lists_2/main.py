97fe359559af575bd5af9895d146e3ba93813d6c
Novikov Petr <baksik.bz@gmail.com>
2023-05-03 12:12:27 +0300

    Задание выполнено.

--- Module21/05_list_of_lists_2/main.py
+++ Module21/05_list_of_lists_2/main.py
@@ -1,4 +1,13 @@
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+def rec(*list):
+    rec_fun = []
+    for elem in list:
+        try:
+            rec_fun +=rec(*elem)
+        except TypeError:
+            rec_fun.append(elem)
+    return rec_fun
+
+print('Ответ:', rec(nice_list))
