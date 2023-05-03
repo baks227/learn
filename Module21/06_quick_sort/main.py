0be8d83dbe103be997187d242a5942154fd20b44
Novikov Petr <baksik.bz@gmail.com>
2023-05-03 12:25:46 +0300

    Задание выполнено.

--- Module21/06_quick_sort/main.py
+++ Module21/06_quick_sort/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+def qsort(arr):
+    if len(arr)<=1:
+        return arr
+    opora = arr[-1]
+    left =[]
+    mid = []
+    right = []
+    for e in arr:
+        if e < opora:
+            left.append(e)
+        elif e == opora:
+            mid.append(e)
+        else:
+            right.append(e)
+    return qsort(left)+mid + qsort(right)
+
+list_num =  [5, 8, 9, 4, 2, 9, 1, 8]
+print(qsort(list_num))
