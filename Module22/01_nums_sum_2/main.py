15b8ea73352f6859fa240daa6b87439d45d2d148
Novikov Petr <baksik.bz@gmail.com>
2023-05-15 11:38:28 +0300

    Задание выполнено.

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+import os
+
+num_file = open('numbers.txt','r')
+sum_num = 0
+data = num_file.read()
+for num in data:
+    if num != ' ' and num != '\n':
+        num = int(num)
+        sum_num += num
+print('Содержимое файла numbers.txt\n',data)
+num_file.close()
+new_file = open('answer.txt','w')
+new_file.write(str(sum_num))
+print('Содержимое файла answer.txt\n',sum_num)
+new_file.close()
